992d8c9b05fe67b3d8aa1e548dd84c2d7a76cb4d
Valentin <valentin.lamine@ynov.com>
2022-11-07 19:28:01 +0100

    Avancement page choix

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function random() {
     var x = Math.floor((Math.random() * 3) + 1);
     if (x == 1) {
-        window.location.href = "overworld/random_1.html";
+        window.location.href = "overworld/desert/index.html";
     } else if (x == 2) {
-        window.location.href = "overworld/random_2.html";
+        window.location.href = "overworld/plaine/index.html";
     } else if (x == 3) {
-        window.location.href = "overworld/random_3.html";
+        window.location.href = "overworld/ile/index.html";
     }
 }
